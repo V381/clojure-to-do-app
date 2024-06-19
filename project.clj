d7a567551a865e2033a602f2fcd9dfaeb3f8beac
Pavle Paunovic <paavlee@yahoo.com>
2024-06-19 04:47:44 +0200

    Uberjar generated

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject your-app-name "0.1.0-SNAPSHOT"
-  :description "Clojure ToDo by Pavle"
+  :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.2"]
+                 [ring/ring-core "1.9.3"]
+                 [ring/ring-jetty-adapter "1.9.3"]
                  [ring/ring-defaults "0.3.2"]
                  [hiccup "1.0.5"]
                  [cheshire "5.10.0"]]
-  :min-lein-version "2.0.0"
-  :source-paths ["src"]
   :main ^:skip-aot your-app-name.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
